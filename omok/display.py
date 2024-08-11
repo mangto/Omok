867f15e5efc472ec17b60a323f268ce7e48cd574
unknown <mangto0701@gmail.com>
2024-08-11 23:58:33 +0900

    display

--- omok/display.py
+++ omok/display.py
@@ -86,32 +86,6 @@ class draw:
         rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
         rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
         return surface.blit(rectangle,pos)
-        
-    def rrect(surface, rect, color, radius=0.4):
-        rect         = pygame.Rect(rect)
-        color        = pygame.Color(*color)
-        alpha        = color.a
-        color.a      = 0
-        pos          = rect.topleft
-        rect.topleft = 0,0
-        rectangle    = pygame.Surface(rect.size,pygame.SRCALPHA)
-        circle       = pygame.Surface([min(rect.size)*3]*2,pygame.SRCALPHA)
-        pygame.draw.ellipse(circle,(0,0,0),circle.get_rect(),0)
-        circle       = pygame.transform.smoothscale(circle,[int(min(rect.size)*radius)]*2)
-        radius              = rectangle.blit(circle,(0,0))
-        radius.bottomright  = rect.bottomright
-        rectangle.blit(circle,radius)
-        radius.topright     = rect.topright
-        rectangle.blit(circle,radius)
-        radius.bottomleft   = rect.bottomleft
-        rectangle.blit(circle,radius)
-
-        rectangle.fill((0,0,0),rect.inflate(-radius.w,0))
-        rectangle.fill((0,0,0),rect.inflate(0,-radius.h))
-
-        rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
-        rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
-        return surface.blit(rectangle,pos)
     
     def text(text:str, font:pygame.Font, window:pygame.Surface, x:int, y:int, cenleft="center", color=(0,0,0)):
         text_obj = font.render(text, True, color)
